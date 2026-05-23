dec8ff752b3b8bd9a364cbac8d39f16e08c9e908
Hala M. Dalloul <haladalloul42@gmail.com>
2026-05-23 11:05:35 +0300

    Implement login screen navigation and handle redirection to the home screen on login success

--- app/src/main/java/com/example/todo_list/ui/navigation/NavGraph.kt
+++ app/src/main/java/com/example/todo_list/ui/navigation/NavGraph.kt
@@ -5,13 +5,18 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.todo_list.ui.auth.LoginScreen
 
 @Composable
 fun NavGraph(){
     val navHostController = rememberNavController()
     NavHost(navHostController, startDestination = Screens.LoginScreen.route) {
         composable(Screens.LoginScreen.route){
-
+            LoginScreen(onLoginSuccess = {
+                navHostController.navigate(Screens.HomeScreen.route) {
+                    popUpTo(Screens.LoginScreen.route) { inclusive = true }
+                }
+            })
         }
         composable(Screens.HomeScreen.route){
 
